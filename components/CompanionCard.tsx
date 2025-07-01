38d0914ae864bbbba422bab62cf839597acf790c
Jacobli6177 <101788026+Jacobli6177@users.noreply.github.com>
2025-06-30 23:59:15 -0400

    modify some fixes for companion cards

--- components/CompanionCard.tsx
+++ components/CompanionCard.tsx
@@ -3,36 +3,34 @@ import Link from 'next/link'
 import React from 'react'
 
 interface CompanionCardsProp {
-    id: string,
-    name: string,
-    topic: string,
-    subject: string,
-    duration: number,
-    color: string,
+  id: string,
+  name: string,
+  topic: string,
+  subject: string,
+  duration: number,
+  color: string,
 }
 
-const CompanionCard = ({id, name, topic, subject, duration, color}: CompanionCardsProp) => {
+const CompanionCard = ({ id, name, topic, subject, duration, color }: CompanionCardsProp) => {
   return (
-    <article className='companion-cards' style= {{ backgroundColor: color}}>
-        <div className='flex justify-between items-center'>
-            <div className='subject-badge'>{subject}</div>
-            <button className='companion-bookmark'>
-                    <Image src="/icons/bookmark.svg" alt= "bookmark" width={12.5} height={15}/>
-            </button>
-        </div>
+    <article className='companion-cards' style={{ backgroundColor: color }}>
+      <div className='flex justify-between items-center'>
+        <div className='subject-badge'>{subject}</div>
+        <button className='companion-bookmark'>
+          <Image src="/icons/bookmark.svg" alt="bookmark" width={12.5} height={15} />
+        </button>
+      </div>
 
-        <h2 className="text-2xl font-bold">{name}</h2>
-        <p className='text-sm'>{topic}</p>
-        <div className='flex items-center gap-2'>
-            <Image src="/icons/clock.svg" alt="duration" width={13.5} height={13.5}/>
-            <p className='text-sm'>{duration} minutes</p>
-        </div>
+      <h2 className="text-2xl font-bold">{name}</h2>
+      <p className='text-sm'>{topic}</p>
+      <div className='flex items-center gap-2'>
+        <Image src="/icons/clock.svg" alt="duration" width={13.5} height={13.5} />
+        <p className='text-sm'>{duration} minutes</p>
+      </div>
 
-        <Link href={`/companions/${id}`} className='w-full'>
-            <button className='btn-primary w-full justify-center'>
-                Launch Lesson
-            </button>
-        </Link>
+      <Link href={`/companions/${id}`} className='btn-primary w-full justify-center text-center'>
+        Launch Lesson
+      </Link>
     </article>
   )
 }
